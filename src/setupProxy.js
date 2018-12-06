8f4ba42a71bf00a1b69e9af1247bfde0f9b2f9f6
0xtinyuk <Leo@Leo-3.local>
2018-12-05 23:27:08 -0500

    proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,8 +1,11 @@
-const proxy = require('http-proxy-middleware');
+const proxy = require("http-proxy-middleware");
 
 module.exports = function(app) {
-  app.use(proxy('/login', { target: 'http://localhost:3001/' }));
-  app.use(proxy('/customer', { target: 'http://localhost:3001/' }));
-  app.use(proxy('/owner/', { target: 'http://localhost:3001/' }));
-  app.use(proxy('/property/', { target: 'http://localhost:3001/' }));
+  app.use(proxy("/login", { target: "http://localhost:3001/" }));
+  app.use(proxy("/logout", { target: "http://localhost:3001/" }));
+  app.use(proxy("/customer", { target: "http://localhost:3001/" }));
+  app.use(proxy("/owner/", { target: "http://localhost:3001/" }));
+  app.use(proxy("/property/", { target: "http://localhost:3001/" }));
+  app.use(proxy("/agent", { target: "http://localhost:3001/" }));
+  app.use(proxy("/rental", { target: "http://localhost:3001/" }));
 };
