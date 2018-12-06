f9a5161e1dfe5f9e8ef73c04df91035d4d3dc8b7
bobster12345 <38799868+bobster12345@users.noreply.github.com>
2018-12-05 22:57:45 -0500

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
     <div>
       <div className="container">
         <h4 className="center">Home</h4>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae repudiandae repellat illo magni eligendi cupiditate voluptates eius nam voluptate. Incidunt nihil ullam quae quia officia quaerat, deserunt eligendi explicabo totam?</p>
+        <p>Welcome to the WFHX system. Please navigate via the menu up top.</p>
       </div>
     </div>
   )
