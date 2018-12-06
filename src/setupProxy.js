baaeb71883d7ef0932f5333e6dd8d5fbb401eab2
bobster12345 <38799868+bobster12345@users.noreply.github.com>
2018-12-05 22:27:59 -0500

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,4 +3,5 @@ const proxy = require('http-proxy-middleware');
 module.exports = function(app) {
   app.use(proxy('/login', { target: 'http://localhost:3001/' }));
   app.use(proxy('/customer', { target: 'http://localhost:3001/' }));
+  app.use(proxy('/owner', { target: 'http://localhost:3001/' }));
 };
